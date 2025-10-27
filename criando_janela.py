6a3203694218a9256cee0b79fff3787e9ba5e30f
Felipe Correia <felipecc9@al.insper.edu.br>
2025-10-27 18:04:57 -0300

    EXERCÍCIO 2 + redimensionamentos

--- criando_janela.py
+++ criando_janela.py
@@ -23,7 +23,12 @@ while game:
             game = False
 
     # ----- Gera saídas
-    window.fill((0, 0, 255))  # Preenche com a cor branca
+    window.fill((0, 200, 0))  # Preenche com a cor verde
+    cor = (255, 240, 0) # Preenche com a cor amarela 
+    vertices = [(300, 0), (600, 150), (300, 300), (0, 150)] # Dimensões do poligono, redimensionadas para se adequar a tela
+    cor_circulo = (0, 0, 200) #deixa azul, não é 255 pq fica muito claro, 200 fica mais escuro
+    pygame.draw.polygon(window, cor, vertices) # Poligono feito
+    pygame.draw.circle(window, cor_circulo, (300, 150), 100) # Fazendo circulo (sobreposição)
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
